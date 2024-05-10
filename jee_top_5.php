3e83f22e0b711978aa55c0ac163aaa57a641af15
Modilover69 <132368904+Modilover69@users.noreply.github.com>
2024-05-10 10:11:22 +0530

    Update jee_top_5.php

--- jee_top_5.php
+++ jee_top_5.php
@@ -99,7 +99,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "asdfghjkl123";
+$password = "<Your password>";
 $dbname = "wtl";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
